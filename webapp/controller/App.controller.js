85e6c25ce6f3cc4f4b674bd2ff7f69d6632aa8bb
Iván Rodrigo <irodrigob@users.noreply.github.com>
2019-12-26 17:45:11 +0100

    Initial upload
    Initial upload

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (
+	Controller
+) {
+	"use strict";
+
+	return Controller.extend("com.ivancio.zal30-ui5.controller.App", {
+	});
+});
---
+++ webapp/controller/viewSelect.controller.js
@@ -0,0 +1,8 @@
+sap.ui.define([
+  "sap/ui/core/mvc/Controller",
+"sap/m/MessageToast"
+], function(Controller, MessageToast) {
+  "use strict";
+
+  return Controller.extend("com.ivancio.zal30-ui5.controller.viewSelect", {});
+});
