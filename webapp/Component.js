17b673e7b066545cf15be5058a41b572c95ae49a
Iván Rodrigo <irodrigob@users.noreply.github.com>
2019-12-27 13:49:39 +0100

    develop

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,7 +2,7 @@ sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/Device",
 	"com/ivancio/zal30-ui5/model/models"
-], function(UIComponent, Device, models) {
+], function (UIComponent, Device, models) {
 	"use strict";
 
 	return UIComponent.extend("com.ivancio.zal30-ui5.Component", {
@@ -16,15 +16,47 @@ sap.ui.define([
 		 * @public
 		 * @override
 		 */
-		init: function() {
+		init: function () {
 			// call the base component's init function
 			UIComponent.prototype.init.apply(this, arguments);
 
 			// enable routing
 			this.getRouter().initialize();
 
-			// set the device model
-			this.setModel(models.createDeviceModel(), "device");
+			// Se inicializa el modelo de datos
+			this._initModelData();
+		},
+		_initModelData: function () {
+			// Se inicializa la variable con el modelo de la aplicación. Este AppData esta
+			// definido en el manifest con la etiqueta "appData". 
+			var oAppDataModel = this.getModel("appData");
+			
+			// Se recupera los parámetros de la URL
+			var oUrlParams = jQuery.sap.getUriParameters().mParams;
+			
+			// Se indica la configuración de donde se recuperarán los datos en SAP.
+			// El primer parámetro es el modelo donde se obtendrán los datos que contendrá dos campos:
+			// 1) Nombre donde esta definido los datos en el manifest para recuperarlos. 2) Modelo de datos donde se guardará
+			// El segundo parámetro es el idioma
+			// El tercer parámetro es si los datos se recuperarán del mock de dos maneras: siempre del mock o dependiendo si el servicio ya esta implementado
+			ModelHandler.setConfig([{
+				name: "masterData",
+				model: this.getModel("masterData")
+			  }
+			], "EN", oUrlParams.mock && oUrlParams.mock[0]);
+
+			// Directorio donde estarán los archivos mock
+			models.setBaseDir("com.ivancio.zal30-ui5.model.mock");
+			
+			// A nivel interno de UI5 se fuerza el uso del ingles
+			sap.ui.getCore().getConfiguration().setLanguage("en");
+			
+			// Carga de las vistas para poder ser seleccionadas
+			this._getViews();
+		},
+		_getViews: function () {
+			oAppDataModel = this.getModel("appData");
+
 		}
 	});
 });
--- webapp/model/models.js
+++ webapp/model/models.js
@@ -1,14 +1,73 @@
 sap.ui.define([
 	"sap/ui/model/json/JSONModel",
-	"sap/ui/Device"
-], function(JSONModel, Device) {
+	"sap/ui/Device",
+	"sap/ui/model/Filter",
+	"sap/m/MessageToast"
+], function (JSONModel, Device, MessageToast, Filter) {
 	"use strict";
+	//////////////////////////////////
+	//                              //
+	//      Internal properties     //
+	//                              //
+	//////////////////////////////////
+	var _sLanguage,
+		_bMock = true,
+		_aoModel = [],
+		_sToken = undefined,
+		_oMockDataModel = new JSONModel(),
+		_baseDir;
 
-	return {
-		createDeviceModel: function() {
-			var oModel = new JSONModel(Device);
-			oModel.setDefaultBindingMode("OneWay");
-			return oModel;
+	//////////////////////////////////
+	//                              //
+	//        List of services      //
+	//                              //
+	//////////////////////////////////
+	var _mService = {
+		getViews: {
+			serviceName: "/getViews",
+			bUseMock: true,
+			mockFile: "/getViews.json",
+			oDataModel: "views"
 		}
 	};
+
+	//////////////////////////////////
+	//                              //
+	//        Public methods        //
+	//                              //
+	//////////////////////////////////
+	var oBaseModelHandler = {
+
+		getModel: function (sModel) {
+			var aoModel = _aoModel.filter(function (oModel) {
+				return oModel.name === sModel;
+			});
+
+			return aoModel[0].model;
+		},
+
+		getOdatalUrl: function (sModelName) {
+			return this.getModel(sModelName).sServiceUrl;
+		},
+
+		getBaseServices: function () {
+			return _mService;
+		},
+
+		setConfig: function (aoModels, sLang, sMock) {
+			_aoModel = aoModels,
+				_sLanguage = sLang;
+			oBaseModelHandler.setMockMode(sMock);
+		},
+
+		getMock: function () {
+			return _bMock;
+		},
+
+		setBaseDir: function (sDir) {
+			_baseDir = sDir;
+		}
+	};
+
+	return oBaseModelHandler;
 });
